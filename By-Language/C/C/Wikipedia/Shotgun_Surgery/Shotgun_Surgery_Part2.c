5c6ab1b6534ce847b3c3311c1bcb30943328e382
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-13 21:00:21 -0700

    Shotgun_Surgery_Part2.c
    Part 2 of the Shotgun surgery example.

---
+++ By-Language/C/C/Wikipedia/Shotgun_Surgery/Shotgun_Surgery_Part2.c
@@ -0,0 +1,16 @@
+void MyFunc() {
+  printf("Entering MyFunc\n");
+  ...
+}
+
+void MyFunc2() {
+  printf("Entering MyFunc2\n");
+  ...
+}
+
+...
+
+void MyFuncN() {
+  printf("Entering MyFuncN\n");
+  ...
+}

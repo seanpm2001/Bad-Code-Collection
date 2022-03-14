fc3a510b08364e09df19f319f66f8749318e547c
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-13 20:54:22 -0700

    Nests_Useless.py [V1]
    Version 1 of this source code example.

---
+++ By-Language/P/Python/Forced/Nests_Useless.py
@@ -0,0 +1,29 @@
+# Start of useless garbage script
+time = int(1)
+loop = int(0)
+while (loop == 0):
+	print("Extremely inefficient and pointless time loop, purposefully making bad source code")
+	if time != 0:
+		print("Time")
+		if time != 0:
+			print("Time")
+			if time != 0:
+				print("Time")
+				if time != 0:
+					print("Time")
+					if time != 0:
+						print("Time")
+						if time != 0:
+							print("Time")
+							if time != 0:
+								print("Time")
+								if time != 0:
+									print("Time")
+									break
+
+else:
+	print("Featured on 'what a waste of time'")
+	noMore = input("Press [ENTER] key to quit")
+	break
+return 0
+# End of useless garbage script
